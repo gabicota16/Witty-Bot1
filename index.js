5792d8636a8349265a9dcfef959781ba88394d76
gabicota16 <134754166+gabicota16@users.noreply.github.com>
2023-09-03 22:54:16 -0300

    Witty - Bot

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { name, author } = require(join(__dirname, "./package.json"));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Witry\nBot', {
+say('Witty\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
